3e2a8edd9eecb24e1001cf335423b3a44249efba
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-02-20 11:38:23 +0530

    SMS
    Updated

--- SMS/src/SERVICE/EnrollmentService.java
+++ SMS/src/SERVICE/EnrollmentService.java
@@ -1,5 +1,12 @@
 package SERVICE;
 
-public interface EnrollmentService {
+import java.util.List;
+import MODEL.Enrollment;
 
+interface EnrollmentService {	
+		public int insertEnrollment(Enrollment s);
+		public int updateEnrollment(Enrollment s, String id,String cid);
+		public int deleteEnrollment(String id, String cid);
+		public List<Enrollment> fetchAll();
+		public Enrollment fetchOneEnrollment(String id, String cid);
 }
---
+++ SMS/src/SERVICE/StudentServiceImpl.java
@@ -0,0 +1,23 @@
+package SERVICE;
+
+import java.util.List;
+
+import MODEL.Student;
+
+public class StudentServiceImpl implements StudentService{
+	public int insertStudent(Student s) {
+		return 0;
+	}
+	public int updateStudent(Student s, String id) {
+		return 0;
+	}
+	public int deleteStudent(String s) {
+		return 0;
+	}
+	public List<Student> fetchAll() {
+		return null;
+	}
+	public Student fetchOneStudent(String s) {
+		return null;
+	}
+}
