7863e8601b534680fabc246d003725b5f5231da3
suniljeevan <36226091+suniljeevan@users.noreply.github.com>
2025-03-27 11:49:53 +0530

    UMS
    Register.html
    Style.css

---
+++ UMS/src/main/java/org/cms/controllers/RegistrationController.java
@@ -0,0 +1,40 @@
+package org.cms.controllers;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class RegistrationController
+ */
+public class RegistrationController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public RegistrationController() {
+        // TODO Auto-generated constructor stub
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#getServletConfig()
+	 */
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
